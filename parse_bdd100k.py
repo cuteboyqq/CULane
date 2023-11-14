1577bf8068f0844bac002610d609f06440209048
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-11-14 15:08:20 +0800

    Add files via upload

--- parse_bdd100k.py
+++ parse_bdd100k.py
@@ -12,6 +12,7 @@ class BDD100K:
         self.dataset_dir = args.data_dir
         self.show_im = args.show_im
         self.split_num = args.split_num
+        self.split_width = args.split_width
         self.show_imcrop = args.show_imcrop
         self.save_imcrop = args.save_imcrop
 
@@ -20,9 +21,11 @@ class BDD100K:
         file_name = file.split(".")[0]
         drivable_file  = file_name + ".png"
         lane_file  = file_name + ".png"
+        detection_file = file_name + ".txt"
         drivable_path = os.path.join(self.dataset_dir,"labels","drivable","colormaps",type,drivable_file)
         lane_path = os.path.join(self.dataset_dir,"labels","lane","colormaps",type,lane_file)
-        return drivable_path,lane_path
+        detection_path = os.path.join(self.dataset_dir,"labels","detection",type,detection_file)
+        return drivable_path,lane_path,detection_path
 
 
     def find_max_value(self,a,b,c):
@@ -56,93 +59,159 @@ class BDD100K:
         return min,index
         
 
+    def Get_Min_y_In_Drivable_Area(self,drivable_path):
+        if not os.path.exists(drivable_path):
+            return 0
+        else:
+            print("drivable_path exists!")
+            drivable_img = cv2.imread(drivable_path)
+            if self.show_im:
+                cv2.imshow("drivable",drivable_img)
+                cv2.waitKey(200)
+            drivable_h,drivable_w = drivable_img.shape[0],drivable_img.shape[1]
+            print(f"drivable_h:{drivable_h},drivable_w:{drivable_w}")
+            p1_w =  int(drivable_w / 3.0)
+            p2_w =  int(drivable_w / 2.0)
+            p3_w =  int( (drivable_w*2.0) / 3.0)
+            p1y,p2y,p3y = 0,0,0
+            y = 0
+            find_small_y = False
+            while(y<=drivable_h-1 and find_small_y == False):
+                if(drivable_img[y][p1_w][0]!=0):
+                    find_small_y=True
+                    p1y = y
+                else:
+                    y+=1
+
+            y = 0
+            find_small_y = False
+            while(y<=drivable_h-1 and find_small_y == False):
+                if(drivable_img[y][p2_w][0]!=0):
+                    find_small_y=True
+                    p2y = y
+                else:
+                    y+=1
+            
+            y = 0
+            find_small_y = False
+            while(y<=drivable_h-1 and find_small_y == False):
+                if(drivable_img[y][p3_w][0]!=0):
+                    find_small_y=True
+                    p3y = y
+                else:
+                    y+=1
+
+            print(f"p1y:{p1y},p2y:{p2y},p3y:{p3y}")
+            min,index = self.find_min_value(p1y,p2y,p3y)
+            print(f"min = {min}, index={index}")
+
+            return min,index
         
+        
+            
+    def Find_Min_Y_Among_All_Vehicle_Bounding_Boxes(self,min,detection_path,img_h,img_w):
+        print(f"h:{img_h} w:{img_w}")
+        with open(detection_path,"r") as f:
+            lines = f.readlines()
+            for line in lines:
+                #print(line)
+                la = line.split(" ")[0]
+                x = int(float(line.split(" ")[1])*img_w)
+                y = int(float(line.split(" ")[2])*img_h)
+                w = int(float(line.split(" ")[3])*img_w)
+                h = int(float(line.split(" ")[4])*img_h)
+                print(f"{la} {x} {y} {w} {h}")
+
+                if int(la)==2 and (y+0)<min:
+                    print(f"y:{y} min:{min}")
+                    min=y
+        return min
 
     def Get_Vanish_Area(self):
         im_path_list = glob.glob(os.path.join(self.im_dir,"*.jpg"))
 
         for i in range(len(im_path_list)):
             print(f"{i}:{im_path_list[i]}")
-            drivable_path,lane_path = self.parse_path(im_path_list[i])
+            drivable_path,lane_path,detection_path = self.parse_path(im_path_list[i])
             print(f"drivable_path:{drivable_path}, \n lane_path:{lane_path}")
-            if os.path.exists(drivable_path):
-                print("drivable_path exists!")
-                drivable_img = cv2.imread(drivable_path)
-                if self.show_im:
-                    cv2.imshow("drivable",drivable_img)
-                    cv2.waitKey(200)
-                drivable_h,drivable_w = drivable_img.shape[0],drivable_img.shape[1]
-                print(f"drivable_h:{drivable_h},drivable_w:{drivable_w}")
-                p1_w =  int(drivable_w / 3.0)
-                p2_w =  int(drivable_w / 2.0)
-                p3_w =  int( (drivable_w*2.0) / 3.0)
-                p1y,p2y,p3y = 0,0,0
-                y = 0
-                find_small_y = False
-                while(y<=drivable_h-1 and find_small_y == False):
-                    if(drivable_img[y][p1_w][0]!=0):
-                        find_small_y=True
-                        p1y = y
-                    else:
-                        y+=1
+            
+            img = cv2.imread(im_path_list[i])
+            h,w = img.shape[0],img.shape[1]
 
-                y = 0
-                find_small_y = False
-                while(y<=drivable_h-1 and find_small_y == False):
-                    if(drivable_img[y][p2_w][0]!=0):
-                        find_small_y=True
-                        p2y = y
-                    else:
-                        y+=1
-                
-                y = 0
-                find_small_y = False
-                while(y<=drivable_h-1 and find_small_y == False):
-                    if(drivable_img[y][p3_w][0]!=0):
-                        find_small_y=True
-                        p3y = y
-                    else:
-                        y+=1
+            min_final,index = self.Get_Min_y_In_Drivable_Area(drivable_path)
 
-                print(f"p1y:{p1y},p2y:{p2y},p3y:{p3y}")
-                min,index = self.find_min_value(p1y,p2y,p3y)
-                print(f"min = {min}, index={index}")
+            min_final_2 = self.Find_Min_Y_Among_All_Vehicle_Bounding_Boxes(min_final,detection_path,h,w)
 
             if os.path.exists(lane_path):
                 print("lane_path exists!")
-            self.split_Image(im_path_list[i],min,i)
+            self.split_Image(im_path_list[i],min_final_2,i)
 
-        return min
+        return min_final_2
     
     def split_Image(self,im_path,drivable_min_y,cnt):
-        print(im_path)
-        label=None
-        img = cv2.imread(im_path)
-        split_y = int(img.shape[0] / self.split_num)
-        h,w = img.shape[0],img.shape[1]
-        print(f"split_y:{split_y}")
-        for i in range(self.split_num):
-            split_img = img[split_y*i:split_y*(i+1),0:w-1]
-            
-            
-            if drivable_min_y>=split_y*i and drivable_min_y<split_y*(i+1):
-                print("it is vansih line area")
-                label=0
+        tag = 0
+        retval = 0
+        if os.path.exists(im_path):
+            print(im_path)
+            label=None
+            y = int(drivable_min_y)
+            print(f"y:{y}")
+            img = cv2.imread(im_path)
+            split_y = int(img.shape[0] / self.split_num)
+            h,w = img.shape[0],img.shape[1]
+            lower_bound = y-int(split_y/2.0)
+            upper_bound = y+int(split_y/2.0)
+            print(f"split_y:{split_y}")
+            if y>split_y:
+                split_vanish_crop_image = img[lower_bound:upper_bound,0:w-1]
+                cv2.imshow("split vanish area",split_vanish_crop_image)
+                cv2.waitKey(100)
                 input()
+                if self.save_imcrop:
+                    label = 0
+                    save_dir = os.path.join(self.save_dir,str(label))
+                    os.makedirs(save_dir,exist_ok=True)
+                    save_crop_im = str(cnt) + "_" + str(tag) + ".jpg"
+                    save_crop_im_path = os.path.join(save_dir,save_crop_im)
+                    cv2.imwrite(save_crop_im_path,split_vanish_crop_image)
+                    tag=tag+1
             else:
-                label=1
+                print("y is too small~~")
+                retval = -1
+                return retval
             
-            if self.save_imcrop:
-                save_img_dir = os.path.join(self.save_dir,str(label))
-                os.makedirs(save_img_dir,exist_ok=True)
-                im_name = str(cnt) + "_" + str(i+1) +  ".jpg"
-                save_im_path = os.path.join(save_img_dir,im_name)
-                cv2.imwrite(save_im_path,split_img)
-
-
-            if self.show_imcrop:
-                cv2.imshow("split img",split_img)
-                cv2.waitKey(400)
+            if y>split_y:
+                y_l = lower_bound - split_y
+                while(y_l>0 and y_l+split_y<h):
+                    split_other_image = img[y_l:y_l+split_y,0:w-1]
+                    # cv2.imshow("up others img",split_other_image)
+                    # cv2.waitKey(100)
+                    #input()
+                    y_l=y_l-split_y
+                    if self.save_imcrop:
+                        label = 1
+                        save_dir = os.path.join(self.save_dir,str(label))
+                        os.makedirs(save_dir,exist_ok=True)
+                        save_crop_im = str(cnt) + "_" + str(tag) + ".jpg"
+                        save_crop_im_path = os.path.join(save_dir,save_crop_im)
+                        cv2.imwrite(save_crop_im_path,split_other_image)
+                        tag=tag+1
+                y_t = upper_bound + split_y
+                while(y_t<(h-1) and y_t-split_y>0):
+                    split_other_image = img[y_t-split_y:y_t,0:w-1]
+                    # cv2.imshow("down others img",split_other_image)
+                    # cv2.waitKey(100)
+                    #input()
+                    y_t = y_t + split_y
+                    if self.save_imcrop:
+                        label = 1
+                        save_dir = os.path.join(self.save_dir,str(label))
+                        os.makedirs(save_dir,exist_ok=True)
+                        save_crop_im = str(cnt) + "_" + str(tag) + ".jpg"
+                        save_crop_im_path = os.path.join(save_dir,save_crop_im)
+                        cv2.imwrite(save_crop_im_path,split_other_image)
+                        tag=tag+1
+            
         return NotImplemented
     
 
@@ -158,6 +227,7 @@ def get_args():
     parser.add_argument('-saveimcrop','--save-imcrop',type=bool,help='save  crop images',default=True)
 
     parser.add_argument('-splitnum','--split-num',type=int,help='split number',default=10)
+    parser.add_argument('-splitwidth','--split-width',type=int,help='split image width',default=72)
     parser.add_argument('-dataset','--dataset',help='dataset directory',default="/home/ali/Projects/datasets/CULane/driver_161_90frame_crop_2cls/train")
     return parser.parse_args()
 
