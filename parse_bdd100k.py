519b2c671f3b8ed833782013e1e395a395836649
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-11-17 20:15:15 +0800

    Add files via upload

--- parse_bdd100k.py
+++ parse_bdd100k.py
@@ -20,6 +20,7 @@ class BDD100K:
         self.shift_pixels = args.shift_pixels
         self.data_type = args.data_type
         self.data_num = args.data_num
+        self.show_vanishline = args.show_vanishline
 
     def parse_path(self,path,type="val"):
         file = path.split(os.sep)[-1]
@@ -116,9 +117,15 @@ class BDD100K:
             
     def Find_Min_Y_Among_All_Vehicle_Bounding_Boxes(self,min,detection_path,img_h,img_w):
         print(f"h:{img_h} w:{img_w}")
+        min_rea = 999999
+        find_min_area=False
+        min_x=99999
+        min_w=99999
+        min_h=99999
         with open(detection_path,"r") as f:
             lines = f.readlines()
             for line in lines:
+                find_min_area=False
                 #print(line)
                 la = line.split(" ")[0]
                 x = int(float(line.split(" ")[1])*img_w)
@@ -126,11 +133,21 @@ class BDD100K:
                 w = int(float(line.split(" ")[3])*img_w)
                 h = int(float(line.split(" ")[4])*img_h)
                 #print(f"{la} {x} {y} {w} {h}")
+                if w*h < min_rea and int(la)==2:
+                    print(f"w*h={w*h},min_rea={min_rea},x:{x},y:{y}")
+                    min_rea = w*h
+                    find_min_area=True
+                    print(f"find_min_area :{find_min_area} ")
+                    
 
-                if int(la)==2 and (y+0)<min:
+                if int(la)==2 and (y+0)<min and find_min_area:
                     print(f"y:{y} min:{min}")
                     min=y
+                    min_x=x
+                    min_w=w
+                    min_h=h
         return min
+        #return min,min_x,min_w,min_h
 
     def Get_Vanish_Area(self):
         '''
@@ -143,6 +160,7 @@ class BDD100K:
             self.im_dir : the image directory
             self.dataset_dir : the dataset directory
             self.save_dir : save crop image directory
+            self.multi_crop : save multiple vanish area crop images
         output:
             the split images
         '''
@@ -159,6 +177,9 @@ class BDD100K:
             print(f"{i}:{im_path_list[i]}")
             drivable_path,lane_path,detection_path = self.parse_path(im_path_list[i],type=self.data_type)
             #print(f"drivable_path:{drivable_path}, \n lane_path:{lane_path}")
+            if not os.path.exists(detection_path):
+                print(f"detection_path not exist !! PASS:{detection_path}")
+                continue
             
             img = cv2.imread(im_path_list[i])
             h,w = img.shape[0],img.shape[1]
@@ -169,10 +190,13 @@ class BDD100K:
 
             # if os.path.exists(lane_path):
             #     print("lane_path exists!")
+            # self.split_Image(im_path_list[i],min_final_2,min_x,min_w,minh)
             self.split_Image(im_path_list[i],min_final_2)
+
 
         return min_final_2
     
+    # def split_Image(self,im_path,drivable_min_y,min_x,min_w,minh):
     def split_Image(self,im_path,drivable_min_y):
         tag = 0
         retval = 0
@@ -211,9 +235,7 @@ class BDD100K:
                 print(f"split_y:{split_y}")
                 if lower_bound>0:
                     split_vanish_crop_image = img[lower_bound:upper_bound,0:w-1]
-                    if self.show_imcrop:
-                        cv2.imshow("split vanish area",split_vanish_crop_image)
-                        cv2.waitKey(1)
+                    
                     #input()
                     if self.save_imcrop:
                         label = 0
@@ -226,6 +248,18 @@ class BDD100K:
                         else:
                             print(f"file {img_name}_0.jpg exists")
                         tag=tag+1
+                    if self.show_imcrop:
+                        if self.show_vanishline:
+                            newImage = img.copy()
+                            cv2.line(newImage, (0, drivable_min_y), (w-1, drivable_min_y), (0, 0, 255), 1)
+                            #cv2.rectangle(newImage, start_point, end_point, color, thickness) 
+                            split_vanish_crop_image = newImage[lower_bound:upper_bound,0:w-1]
+                            cv2.imshow("split vanish area",split_vanish_crop_image)
+                            cv2.waitKey(1)
+                            input()
+                        else:
+                            cv2.imshow("split vanish area",split_vanish_crop_image)
+                            cv2.waitKey(1)
                 else:
                     print("y is too small~~")
                     retval = -1
@@ -277,21 +311,22 @@ def get_args():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-imdir','--im-dir',help='image directory',default="/home/ali/Projects/datasets/BDD100K-ori/images/100k/train")
-    parser.add_argument('-savedir','--save-dir',help='save image directory',default="/home/ali/Projects/datasets/BDD100K_Train_crop")
+    parser.add_argument('-savedir','--save-dir',help='save image directory',default="/home/ali/Projects/datasets/BDD100K_Train_Crop_20000_Ver2")
     parser.add_argument('-datadir','--data-dir',help='dataset directory',default="/home/ali/Projects/datasets/BDD100K-ori")
 
     parser.add_argument('-datatype','--data-type',help='data type',default="train")
-    parser.add_argument('-datanum','--data-num',type=int,help='number of images to crop',default=10000)
+    parser.add_argument('-datanum','--data-num',type=int,help='number of images to crop',default=20000)
 
 
 
     parser.add_argument('-showim','--show-im',type=bool,help='show images',default=False)
     parser.add_argument('-showimcrop','--show-imcrop',type=bool,help='show crop images',default=True)
+    parser.add_argument('-showvanishline','--show-vanishline',type=bool,help='show vanish line in image',default=False)
     parser.add_argument('-saveimcrop','--save-imcrop',type=bool,help='save  crop images',default=True)
 
     parser.add_argument('-multicrop','--multi-crop',type=bool,help='save multiple vanish area crop images',default=True)
-    parser.add_argument('-multinum','--multi-num',type=int,help='number of multiple vanish area crop images',default=8)
-    parser.add_argument('-shiftpixel','--shift-pixels',type=int,help='number of multiple crop images shift pixels',default=3)
+    parser.add_argument('-multinum','--multi-num',type=int,help='number of multiple vanish area crop images',default=6)
+    parser.add_argument('-shiftpixel','--shift-pixels',type=int,help='number of multiple crop images shift pixels',default=2)
 
     parser.add_argument('-splitnum','--split-num',type=int,help='split number',default=10)
     parser.add_argument('-splitwidth','--split-width',type=int,help='split image width',default=72)
